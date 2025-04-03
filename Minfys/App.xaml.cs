96bd5a7033a347249ebb4c9c6ef7acb9afc1c17d
Ghost <mobicom.dg@proton.me>
2025-04-03 15:39:50 -0600

    build(app): add .NET generic host
    Add .NET Generic Host dependency and implement default host functionality in App.xaml.cs.

--- Minfys/App.xaml.cs
+++ Minfys/App.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Documents;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Minfys.ViewModels;
 
 namespace Minfys;
 
@@ -9,4 +13,43 @@ namespace Minfys;
 /// </summary>
 public partial class App : Application
 {
+    private IHost _host;
+
+    public App()
+    {
+        Host.CreateDefaultBuilder().ConfigureServices(service =>
+        {
+            service.AddSingleton<MainWindow>();
+            
+
+        }).Build();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        _host.Start();
+
+        var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+        
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        try
+        {
+            await _host.StopAsync();
+        }
+        catch (Exception exception)
+        {
+            Console.Error.WriteLine("Error while stopping host: " + exception.Message);
+        }
+        finally
+        {
+            _host.Dispose();
+        }
+        
+        base.OnExit(e);
+    }
 }
