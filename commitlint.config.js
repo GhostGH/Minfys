b45a11b1406ccb82855490579c80ad0d2debb9be
Ghost <mobicom.dg@proton.me>
2025-04-03 14:08:28 -0600

    chore(config): add scopes for commitlint
    Add a list of mandatory scopes to make commits easier to understand.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,16 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    'rules': {
+    'scope-enum': [2, 'always', [
+        'app',
+        'views',
+        'viewmodels',
+        'models',
+        'services',
+        'resources',
+        'build',
+        'ci',
+        'config'
+    ]],
+    },
+};
