6a54bef1b6ad2d56d685b6a370661cdb4a27731a
Ghost <mobicom.dg@proton.me>
2025-07-11 20:21:39 -0600

    docs: document AudioOptions.cs

--- Minfys/Models/Options/AudioOptions.cs
+++ Minfys/Models/Options/AudioOptions.cs
@@ -1,15 +1,25 @@
 ﻿namespace Minfys.Models.Options;
 
 /// <summary>
-/// Contains timer song settings.
+/// Contains settings related to audio playback for timer notifications.
 /// </summary>
 public class AudioOptions
 {
     public const string Key = "AudioOptions";
 
+    /// <summary>
+    /// Full path to the audio file that will be played as a notification.
+    /// </summary>
     public string FilePath { get; set; } =
         "C://Programming projects//C#//Projects//Minfys//Minfys//Assets//Audio//quest_ding_1.mp3";
 
+    /// <summary>
+    /// Indicates whether the audio should loop when played.
+    /// </summary>
     public bool LoopEnabled { get; set; } = false;
+
+    /// <summary>
+    /// Volume level of the notification sound, ranging from 0.0 (mute) to 1.0 (maximum).
+    /// </summary>
     public float Volume { get; set; } = 0.3f;
 }
