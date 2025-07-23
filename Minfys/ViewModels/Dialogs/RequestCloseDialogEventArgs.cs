c1911ea98d297444fd4ec4dbafb4ec3e22b425ed
Ghost <mobicom.dg@proton.me>
2025-07-22 19:41:44 -0600

    docs: document RequestCloseDialogEventArgs.cs

--- Minfys/ViewModels/Dialogs/RequestCloseDialogEventArgs.cs
+++ Minfys/ViewModels/Dialogs/RequestCloseDialogEventArgs.cs
@@ -1,10 +1,19 @@
 ﻿namespace Minfys.ViewModels.Dialogs;
 
+/// <summary>
+/// Serves for passing arguments for IRequestCloseViewModel.
+/// </summary>
+/// <typeparam name="TResult">Type of additional optional data to pass to the caller from the dialog.</typeparam>
 public class RequestCloseDialogEventArgs<TResult> : EventArgs
 {
     public bool? DialogResult { get; }
     public TResult? Result { get; }
 
+    /// <summary>
+    /// Creates arguments to pass to the caller when the dialog closes.
+    /// </summary>
+    /// <param name="dialogResult">Whether dialog was operation was accepted or canceled.</param>
+    /// <param name="result">Additional optional data to pass to the caller from the dialog.</param>
     public RequestCloseDialogEventArgs(bool? dialogResult, TResult? result = default)
     {
         DialogResult = dialogResult;
