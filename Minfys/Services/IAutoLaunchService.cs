293fce91923c7dbc64ecc467c1807604b9d99a30
Ghost <mobicom.dg@proton.me>
2025-07-22 18:45:19 -0600

    docs: document IAutoLaunchService.cs

--- Minfys/Services/IAutoLaunchService.cs
+++ Minfys/Services/IAutoLaunchService.cs
@@ -1,5 +1,8 @@
 ﻿namespace Minfys.Services;
 
+/// <summary>
+/// Provides contract for implementing app's auto launch functionality.
+/// </summary>
 public interface IAutoLaunchService
 {
     void SetAutoLaunch(bool enabled);
